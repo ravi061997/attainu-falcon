cd9504f2884aed58fbcb30f28fe27808f433b4b5
ravi061997 <39689655+ravi061997@users.noreply.github.com>
2019-05-22 10:53:21 +0530

    Update script.js

--- coding-challenges/challenge 5/script.js
+++ coding-challenges/challenge 5/script.js
@@ -1,1 +1,20 @@
 
+function starPattern(n) {
+  var i,j;
+  for(i=1; i<=n; i++) {
+    var string = "";
+    for(j=1; j<=i; j++) {
+      string = string + "*";
+    }
+    console.log(string);
+  }
+  for(i=n-1; i>=1; i=i--) {
+    var string ="";
+    for(j=1;j<=i;j++) {
+      string = string + "*";
+    }
+    console.log(string);
+  }
+
+}
+starPattern(5);
