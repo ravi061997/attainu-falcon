611a13921a56ff918ad7d70f297f3b2f7da5b478
ravi061997 <39689655+ravi061997@users.noreply.github.com>
2019-06-07 01:16:59 +0530

    Create script.js

---
+++ coding-challenges/challenge13/script.js
@@ -0,0 +1,14 @@
+function checkPalindrome(string) {
+  var n=string.length;
+  for(var i=0; i<Math.floor(n/2); i++) {
+    console.log(string.charAt(i)+ ''+ string.charAt(n-i-1));
+
+    if(string.charAt(i) != string.charAt(n-i-1)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(checkPalindrome("kayak"));
+console.log(checkPalindrome("malayalam"));
