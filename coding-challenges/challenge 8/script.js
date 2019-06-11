01f457c58fb69acea61527b7cd389f664403ac2a
ravi061997 <39689655+ravi061997@users.noreply.github.com>
2019-06-11 22:55:16 +0530

    Update script.js

--- coding-challenges/challenge 8/script.js
+++ coding-challenges/challenge 8/script.js
@@ -1,31 +1,31 @@
- 
-document.getElementById('search').addEventListener('click', function() {
-  var lang = document.querySelector("input").value;
-  searchBooks(lang);
 
-});
+document.getElementById('search').addEventListener('click' , function() {
+  
+  var lang = document.querySelector('input').value;
+  searchBooks(lang);
+})
 function searchBooks(lang) {
   $.ajax ({
-  url: "https://github.com/attainu-falcon/attainu-falcon/blob/master/coding-challenges/data/books.json",
+  url: "https://raw.githubusercontent.com/attainu-falcon/attainu-falcon/master/coding-challenges/data/books.json",
   dataType: "json",
   success: function(books) {
-    $('body').remove();
+    $('table').remove();
     $('body').append("<table><tr><th>Title</th><th>Author</th><th>Country</th><th>Language</th><th>Link</th><th>Pages</th><th>Year</th></tr></table>");
 
     $('table').addClass('table table-striped');
     var i;
-    for(i=0; i<back.length; i++) {
+    for(i=0; i<books.length; i++) {
       if (books[i].language.indexOf(lang) != -1) {
-        //console.log(books[i]);
-        var row = '<tr>';
-        row = row + '<td>' + books[i].Title + '</td>';
-        row = row + '<td>' + books[i].Author + '</td>';
-        row = row + '<td>' + books[i].Countary + '</td>';
-        row = row + '<td>' + books[i].Language + '</td>';
-        row = row + '<td>' + books[i].Link + '</td>';
-        row = row + '<td>' + books[i].Pages + '</td>';
-        row = row + '<td>' + books[i].Year + '</td>';
-         var row = row +'<tr>';
+       //console.log(books[i]);
+        var row =  '<tr>';
+        row = row + '<td>' + books[i].title + '</td>';
+        row = row + '<td>' + books[i].author + '</td>';
+        row = row + '<td>' + books[i].country + '</td>';
+        row = row + '<td>' + books[i].language + '</td>';
+        row = row + '<td>' + books[i].link + '</td>';
+        row = row + '<td>' + books[i].pages + '</td>';
+        row = row + '<td>' + books[i].year + '</td>';
+        row = row +'</tr>';
 
         $('table').append(row);
       }
