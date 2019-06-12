5a719a8c73fbb8ad5d144cf8ace60d00a159a071
ravi061997 <ravendraprajapati1997@gmail.com>
2019-06-13 01:47:49 +0530

    Express_Twitter

--- assignments/Express_Twitter/index.js
+++ assignments/Express_Twitter/index.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-var tweets = [
+var twitt = [
     {
         name: "digitizer",
         tweet: "This is a cool piece of information."
@@ -16,13 +16,13 @@ var tweets = [
 app.use(bodyParser.urlencoded());
 
  app.use(express.static('/public'));
-app.get('/getTweets' , function(req , res) {
-    res.json('tweets');
+app.get('/getTwitters' , function(req , res) {
+    res.json('twitters');
 });
 
 app.post('/' , function(req,res){
     console.log(req.body); 
-    tweets.push(req,body);
+    twitts.push(req,body);
     res.redirect('/')
 })
 
