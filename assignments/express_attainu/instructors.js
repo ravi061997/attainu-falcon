7bc2d5560838f0956ab055978a68f8886148d725
ravi061997 <ravendraprajapati1997@gmail.com>
2019-06-17 02:00:23 +0530

    express_attainu added

--- assignments/express_attainu/instructors.js
+++ assignments/express_attainu/instructors.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req,res){
-    de.collection('students').find().toArray(function(err,result){
+    var db = req.app.locals.db;
+    db.collection('instructors').find().toArray(function(err,result){
         if(err) throw err;
         res.json(result);
 
--- assignments/express_attainu/students.js
+++ assignments/express_attainu/students.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req,res){
-    de.collection('students').find().toArray(function(err,result){
-        if(err) throw err;
-        res.json(result);
+// router.get('/',function(req,res){
+//     res.send("Get route");
+// })
 
+router.get('/', function(req,res){
+    var db = req.app.locals.db;
+    db.collection('students').find().toArray(function(error,result){
+        if(error) throw err;
+        var router = express.Router();
+        res.json(result);
     });
 });
 router.get('/add',function(req,reqs){
@@ -13,7 +18,12 @@ router.get('/add',function(req,reqs){
 });
 
 router.delete('/delete', function(req,res){
-    res.redirect('/');
+    var db = req.app.locals.db;
+    db.collection('students').deleteOne({"_id":req.app.locals.objectId("5d069afa6f849509b6c63e63")},function(error,result){
+        if(error) throw err;
+        var router = express.Router();
+        res.send("Document Deleted");
+    });
 });
 
 router.get('/*',function(req,res){
